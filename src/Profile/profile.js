5dd5424d163ac582ce141b2bd03efa7ed9223f99
WASSIM MELIANI <87248518+MELIANI2@users.noreply.github.com>
2023-02-22 19:58:17 +0100

    propscheckpoints

--- src/Profile/profile.js
+++ src/Profile/profile.js
@@ -1,14 +1,13 @@
-
 import React from 'react';
 
 const Profile = ({ fullName, bio, profession, children, handleName }) => {
   return (
     <div style={{ backgroundColor: '#f5f5f5', padding: 20 }}>
       <h1 style={{ color: 'blue' }}>My Profile</h1>
-      <img src={children} alt="Profile" style={{ width: 200, height: 200, borderRadius: 100 }} />
-      <p style={{ color: 'green', fontSize: 20 }}>Full Name: {fullName}</p>
-      <p style={{ color: 'green', fontSize: 20 }}>Bio: {bio}</p>
-      <p style={{ color: 'green', fontSize: 20 }}>Profession: {profession}</p>
+      <img src={children} alt="Profile" />
+      <p>Full Name: {fullName}</p>
+      <p>Bio: {bio}</p>
+      <p>Profession: {profession}</p>
       <button onClick={() => handleName(fullName)}>Click me</button>
     </div>
   );
