3baa509e608716d627a6d255ff37651de90e1d45
WASSIM MELIANI <87248518+MELIANI2@users.noreply.github.com>
2023-02-22 19:54:23 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import profileImage from './assets/4d7edddc07bd69064cc9c2cf6ae65945453cb94066452ce27c4b4c4e3b064d8b._V_SX1080_.jpeg';
+import Profile from './Profile/profile';
 
 function App() {
+  const handleName = (name) => {
+    alert(`My name is ${name}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile fullName="Joe Black" bio="Nothing" profession="Death" handleName={handleName}>
+        <img src={profileImage} alt="Profile" />
+      </Profile>
     </div>
   );
 }
